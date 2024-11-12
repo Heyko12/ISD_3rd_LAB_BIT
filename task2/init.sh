515fd50e1558e13eed00449a7a8691b0c7ecbc68
Heyko12 <antipova.nk@phystech.edu>
2024-11-12 22:26:29 +0300

    --no-cache added

--- task2/init.sh
+++ task2/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker rm $(docker ps -a -q)
 docker-compose down -v
-docker build -t consumer -f Dockerfile_consumer .
-docker build -t producer -f Dockerfile_producer .
-docker build -t mysql -f Dockerfile_mysql .
+docker build --no-chache -t consumer -f Dockerfile_consumer .
+docker build --no-cache -t producer -f Dockerfile_producer .
+docker build --no-cache -t mysql -f Dockerfile_mysql .
 docker-compose -p 'proj' up --force-recreate
